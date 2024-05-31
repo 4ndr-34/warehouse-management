1d535da4f487e02de75948212baa61b2f61bba1d
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-05-31 21:50:18 +0200

    added repository layer functionalitites

--- src/main/java/com/backend/warehouse_management/repository/UserRepository.java
+++ src/main/java/com/backend/warehouse_management/repository/UserRepository.java
@@ -1,2 +1,14 @@
-package com.backend.warehouse_management.repository;public interface UserRepository {
+package com.backend.warehouse_management.repository;
+
+import com.backend.warehouse_management.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select user from User user where user.email= :email")
+    Optional<User> findByEmail(String email);
+
 }
