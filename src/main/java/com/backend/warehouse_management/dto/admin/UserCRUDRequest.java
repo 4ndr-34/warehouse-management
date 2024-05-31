5eb3369765b645443808cffc4d9f2277599eb6bf
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-05-31 21:49:33 +0200

    basic user interaction dtos

--- src/main/java/com/backend/warehouse_management/dto/admin/UserCRUDRequest.java
+++ src/main/java/com/backend/warehouse_management/dto/admin/UserCRUDRequest.java
@@ -1,2 +1,27 @@
-package com.backend.warehouse_management.dto;public class CreateUserRequest {
+package com.backend.warehouse_management.dto.admin;
+
+
+import com.backend.warehouse_management.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCRUDRequest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
 }
