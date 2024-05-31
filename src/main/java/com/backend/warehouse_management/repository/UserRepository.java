7b118f9302aa4ca39ea90a2fed6edebc8c603554
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-05-31 21:52:10 +0200

    repository annotation

--- src/main/java/com/backend/warehouse_management/repository/UserRepository.java
+++ src/main/java/com/backend/warehouse_management/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.backend.warehouse_management.repository;
 import com.backend.warehouse_management.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("select user from User user where user.email= :email")
