f923530fd4d3d95625d281867bc2d032845e51a7
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-05-31 21:52:36 +0200

    user mapper implementation

--- src/main/java/com/backend/warehouse_management/mapper/UserMapper.java
+++ src/main/java/com/backend/warehouse_management/mapper/UserMapper.java
@@ -1,2 +1,14 @@
-package com.backend.warehouse_management.mapper;public interface UserMapper {
+package com.backend.warehouse_management.mapper;
+
+import com.backend.warehouse_management.dto.admin.UserCRUDRequest;
+import com.backend.warehouse_management.dto.admin.UserCRUDResponse;
+import com.backend.warehouse_management.entity.User;
+import org.mapstruct.*;
+
+@Mapper
+public interface UserMapper {
+
+    User userRequestToUser(UserCRUDRequest userCRUDRequest);
+
+    UserCRUDResponse userToUserResponse(User user);
 }
