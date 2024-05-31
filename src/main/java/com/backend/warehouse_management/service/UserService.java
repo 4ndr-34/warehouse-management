35c608fa62d5b17703ffdb911531558fa2742e08
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-05-31 21:50:46 +0200

    service layer implementation for admin user

--- src/main/java/com/backend/warehouse_management/service/UserService.java
+++ src/main/java/com/backend/warehouse_management/service/UserService.java
@@ -1,2 +1,15 @@
-package com.backend.warehouse_management.service;public interface UserService {
+package com.backend.warehouse_management.service;
+
+import com.backend.warehouse_management.dto.admin.UserCRUDRequest;
+import com.backend.warehouse_management.dto.admin.UserCRUDResponse;
+
+public interface UserService {
+
+    //system admin functionalities
+    UserCRUDResponse adminCreateUser(UserCRUDRequest createUserRequest) throws Exception;
+    UserCRUDResponse adminUpdateUser(UserCRUDRequest userCRUDRequest, Long id);
+    UserCRUDResponse adminGetUserById(Long id);
+    void adminDeleteUser(Long id);
+
+
 }
--- src/main/java/com/backend/warehouse_management/service/impl/UserServiceImpl.java
+++ src/main/java/com/backend/warehouse_management/service/impl/UserServiceImpl.java
@@ -1,2 +1,74 @@
-package com.backend.warehouse_management.service.impl;public class UserServiceImpl {
+package com.backend.warehouse_management.service.impl;
+
+import com.backend.warehouse_management.dto.admin.UserCRUDRequest;
+import com.backend.warehouse_management.dto.admin.UserCRUDResponse;
+import com.backend.warehouse_management.entity.User;
+import com.backend.warehouse_management.mapper.UserMapper;
+import com.backend.warehouse_management.repository.UserRepository;
+import com.backend.warehouse_management.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserCRUDResponse adminCreateUser(UserCRUDRequest createUserRequest) throws Exception {
+        if(userRepository.findByEmail(createUserRequest.getEmail()).isPresent()){
+            throw new Exception("User already exists!");
+        } else {
+            //TODO - when implementing security, encrypt password
+            userRepository.save(userMapper.userRequestToUser(createUserRequest));
+        }
+        return userMapper.userToUserResponse(userRepository.findByEmail(createUserRequest.getEmail()).get());
+    }
+
+    @Override
+    public UserCRUDResponse adminUpdateUser(UserCRUDRequest userCRUDRequest, Long id) {
+        User user = userRepository.findById(id).get();
+
+        return userMapper.userToUserResponse(
+                userRepository.save(
+                        updateUserFromUserRequest(userCRUDRequest, user)));
+    }
+
+    private User updateUserFromUserRequest(UserCRUDRequest userCRUDRequest, User user) {
+
+        if(userCRUDRequest.getFirstName() != null) {
+            user.setFirstName(userCRUDRequest.getFirstName());
+        }
+
+        if(userCRUDRequest.getLastName() != null) {
+            user.setLastName(userCRUDRequest.getLastName());
+        }
+        if(userCRUDRequest.getEmail() != null) {
+            user.setEmail(userCRUDRequest.getEmail());
+        }
+        if(userCRUDRequest.getRole() != null) {
+            user.setRole(userCRUDRequest.getRole());
+        }
+
+        return user;
+    }
+
+    @Override
+    public UserCRUDResponse adminGetUserById(Long id) {
+        User user = userRepository.findById(id).get();
+        return userMapper.userToUserResponse(user);
+    }
+
+    @Override
+    public void adminDeleteUser(Long id) {
+        log.info("deleting user with id: " + id);
+        userRepository.delete(userRepository.findById(id).get());
+    }
 }
