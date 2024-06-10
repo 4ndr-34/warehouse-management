367e371c52c9b6a1311bb44ca04d6a542206d085
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-06-10 18:43:01 +0200

    created a custom order item entity mapper for client and manager

--- src/main/java/com/backend/warehouse_management/mapper/CustomOrderItemMapper.java
+++ src/main/java/com/backend/warehouse_management/mapper/CustomOrderItemMapper.java
@@ -1,2 +1,17 @@
-package com.backend.warehouse_management.mapper;public class customOrderItemMapper {
+package com.backend.warehouse_management.mapper;
+
+import com.backend.warehouse_management.dto.client.OrderItemBasicDetailsDTO;
+import com.backend.warehouse_management.entity.OrderItem;
+
+public class CustomOrderItemMapper {
+
+    public static OrderItemBasicDetailsDTO getBasicDetailsFromOrderItem(OrderItem orderItem) {
+        OrderItemBasicDetailsDTO orderItemBasicDetailsDTO = new OrderItemBasicDetailsDTO();
+        orderItemBasicDetailsDTO.setItemName(orderItem.getItemName());
+        orderItemBasicDetailsDTO.setItemPrice(orderItem.getPrice());
+        orderItemBasicDetailsDTO.setQuantity(orderItem.getQuantity());
+        return orderItemBasicDetailsDTO;
+    }
+
+
 }
