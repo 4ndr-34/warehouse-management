0a2d0b4fc12fd0db41fe12c4aa66ae61012f67ba
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-06-10 19:29:41 +0200

    service layer implementation of general methods for orders

--- src/main/java/com/backend/warehouse_management/service/OrderService.java
+++ src/main/java/com/backend/warehouse_management/service/OrderService.java
@@ -1,2 +1,18 @@
-package com.backend.warehouse_management.service;public interface OrderService {
+package com.backend.warehouse_management.service;
+
+import com.backend.warehouse_management.dto.client.OrderDTO;
+import com.backend.warehouse_management.dto.client.OrderItemDTO;
+import com.backend.warehouse_management.enums.OrderStatus;
+
+import java.util.List;
+
+public interface OrderService {
+
+    OrderDTO createNewOrder(Long id, OrderDTO orderDTO);
+
+    OrderDTO addItemToOrder(Long userId, OrderItemDTO orderItemDTO);
+
+    List<OrderDTO> getAllOrders(Long userId);
+
+    List<OrderDTO> getOrdersByStatus(Long userId, OrderStatus orderStatus);
 }
--- src/main/java/com/backend/warehouse_management/service/impl/OrderServiceImpl.java
+++ src/main/java/com/backend/warehouse_management/service/impl/OrderServiceImpl.java
@@ -1,2 +1,51 @@
-package com.backend.warehouse_management.service.impl;public class OrderServiceImpl {
+package com.backend.warehouse_management.service.impl;
+
+import com.backend.warehouse_management.dto.client.OrderDTO;
+import com.backend.warehouse_management.dto.client.OrderItemDTO;
+import com.backend.warehouse_management.entity.Order;
+import com.backend.warehouse_management.enums.OrderStatus;
+import com.backend.warehouse_management.mapper.CustomOrderMapper;
+import com.backend.warehouse_management.repository.OrderRepository;
+import com.backend.warehouse_management.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+
+    private final OrderRepository orderRepository;
+    @Override
+    public OrderDTO createNewOrder(Long id, OrderDTO orderDTO) {
+        return null;
+    }
+
+    @Override
+    public OrderDTO addItemToOrder(Long userId, OrderItemDTO orderItemDTO) {
+        return null;
+    }
+
+    @Override
+    public List<OrderDTO> getAllOrders(Long userId) {
+        List<Order> ordersForUser = orderRepository.findAllByUserId(userId);
+        List<OrderDTO> orderListForReturn = new ArrayList<>();
+        for(Order order : ordersForUser) {
+            orderListForReturn.add(CustomOrderMapper.clientMapOrderToOrderDTO(order));
+        }
+        return orderListForReturn;
+    }
+
+    @Override
+    public List<OrderDTO> getOrdersByStatus(Long userId, OrderStatus orderStatus) {
+        List<Order> ordersOfStatus = orderRepository.findAllByUserIdAndOrderStatus(userId, orderStatus);
+        List<OrderDTO> orderListForReturn = new ArrayList<>();
+        for(Order order : ordersOfStatus) {
+            orderListForReturn.add(CustomOrderMapper.clientMapOrderToOrderDTO(order));
+        }
+        return orderListForReturn;
+    }
 }
