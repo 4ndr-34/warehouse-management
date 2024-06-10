8e997a5bc8f2ace38a608180e9d206a511145eec
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-06-10 19:26:54 +0200

    method to find order item by product and order id

--- src/main/java/com/backend/warehouse_management/repository/OrderItemRepository.java
+++ src/main/java/com/backend/warehouse_management/repository/OrderItemRepository.java
@@ -4,6 +4,11 @@ import com.backend.warehouse_management.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface OrderItemsRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    Optional<OrderItem> findByProductIdAndOrderId(Long productId, Long orderId);
+
 }
