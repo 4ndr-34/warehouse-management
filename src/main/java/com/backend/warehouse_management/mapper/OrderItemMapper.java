e05b61fa2f91c40b692599e6ad01bb9352d9e1ee
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-06-10 18:43:46 +0200

    changes on these, they will probably get deleted

--- src/main/java/com/backend/warehouse_management/mapper/OrderItemMapper.java
+++ src/main/java/com/backend/warehouse_management/mapper/OrderItemMapper.java
@@ -1,2 +1,13 @@
-package com.backend.warehouse_management.mapper;public class OrderItemMapper {
+package com.backend.warehouse_management.mapper;
+
+import com.backend.warehouse_management.dto.client.OrderItemDTO;
+import com.backend.warehouse_management.entity.OrderItem;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface OrderItemMapper {
+
+    OrderItemDTO orderItemToOrderItemDTO(OrderItem orderItem);
+
+    OrderItem orderItemDTOToOrderItem(OrderItemDTO orderItemDTO);
 }
