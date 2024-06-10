ee4746f7b459349d5eabe1e44069269ae992063b
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-06-10 19:26:09 +0200

    client functionalities dto's

--- src/main/java/com/backend/warehouse_management/dto/client/AddItemToOrderRequest.java
+++ src/main/java/com/backend/warehouse_management/dto/client/AddItemToOrderRequest.java
@@ -1,2 +1,16 @@
-package com.backend.warehouse_management.dto.client;public class AddItemToOrderRequest {
+package com.backend.warehouse_management.dto.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddItemToOrderRequest {
+
+    private Long productId;
+    private Integer quantity;
+    private Long orderId;
+
 }
