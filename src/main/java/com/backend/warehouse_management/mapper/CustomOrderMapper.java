6c4646ed43d6bb2f2b9070dba4d0f70f0287ef13
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-06-10 18:43:18 +0200

    created a custom order entity mapper for client and manager

--- src/main/java/com/backend/warehouse_management/mapper/CustomOrderMapper.java
+++ src/main/java/com/backend/warehouse_management/mapper/CustomOrderMapper.java
@@ -1,2 +1,33 @@
-package com.backend.warehouse_management.mapper;public class CustomOrderMapper {
+package com.backend.warehouse_management.mapper;
+
+import com.backend.warehouse_management.dto.client.OrderDTO;
+import com.backend.warehouse_management.entity.Order;
+
+import java.util.stream.Collectors;
+
+public class CustomOrderMapper {
+
+
+    public static OrderDTO clientMapOrderToOrderDTO(Order order) {
+        OrderDTO orderDTO = new OrderDTO();
+
+        orderDTO.setOrderNumber(order.getOrderNumber());
+        orderDTO.setOrderStatus(order.getOrderStatus());
+        orderDTO.setUserId(order.getUser().getId());
+        if(order.getDelivery() != null){
+            orderDTO.setDeliveryId(order.getDelivery().getId());
+        }
+
+        orderDTO.setDeadline(order.getDeadline());
+
+        if(order.getOrderItems() != null){
+            orderDTO.setOrderItems(order.getOrderItems()
+                    .stream()
+                    .map(CustomOrderItemMapper::getBasicDetailsFromOrderItem)
+                    .collect(Collectors.toList()));
+        }
+        orderDTO.setTotalPrice(order.getTotalPrice());
+        return orderDTO;
+    }
+
 }
