53733b4e9e197f3d9d475af1acf23339f8832370
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-06-12 21:39:04 +0200

    added cascade type for relationship with delivery

--- src/main/java/com/backend/warehouse_management/entity/Order.java
+++ src/main/java/com/backend/warehouse_management/entity/Order.java
@@ -1,6 +1,7 @@
 package com.backend.warehouse_management.entity;
 
 import com.backend.warehouse_management.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -20,6 +21,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private UUID orderNumber;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate submittedDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
@@ -27,7 +29,7 @@ public class Order {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems;
     private LocalDate deadline;
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.MERGE)
     @JoinColumn(name = "delivery_id", referencedColumnName = "id")
     private Delivery delivery;
     @ManyToOne(cascade = CascadeType.MERGE)
