06ada43d84d9ba40fc10a987b49a0e1941ba57cf
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-06-12 21:40:22 +0200

    date formatter and field refactoring

--- src/main/java/com/backend/warehouse_management/dto/manager/DeliveryDTO.java
+++ src/main/java/com/backend/warehouse_management/dto/manager/DeliveryDTO.java
@@ -1,6 +1,7 @@
 package com.backend.warehouse_management.dto.manager;
 
 import com.backend.warehouse_management.dto.client.OrderDTO;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +14,10 @@ import java.util.List;
 @NoArgsConstructor
 public class DeliveryDTO {
 
+    private Long deliveryId;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate scheduledDate;
     private List<OrderDTO> orders;
-    private TruckDTO truck;
+    private Long truckId;
     private Double remainingSpace;
 }
