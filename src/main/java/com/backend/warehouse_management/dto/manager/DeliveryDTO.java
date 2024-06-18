7a39efcda9a3ebfce4e5e9e2792349df1e55486f
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-06-18 22:55:13 +0200

    removal of constructor annotations

--- src/main/java/com/backend/warehouse_management/dto/manager/DeliveryDTO.java
+++ src/main/java/com/backend/warehouse_management/dto/manager/DeliveryDTO.java
@@ -2,16 +2,12 @@ package com.backend.warehouse_management.dto.manager;
 
 import com.backend.warehouse_management.dto.client.OrderDTO;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class DeliveryDTO {
 
     private Long deliveryId;
