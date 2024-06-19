c3a9ce1e8084d5c61b4ee49c7c9fcc22cb04e448
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-06-19 21:17:53 +0200

    moving all order business logic to OrderService

--- src/main/java/com/backend/warehouse_management/service/OrderService.java
+++ src/main/java/com/backend/warehouse_management/service/OrderService.java
@@ -1,18 +1,30 @@
 package com.backend.warehouse_management.service;
 
-import com.backend.warehouse_management.dto.client.OrderDTO;
-import com.backend.warehouse_management.dto.client.OrderItemDTO;
+import com.backend.warehouse_management.dto.client.*;
+import com.backend.warehouse_management.dto.manager.CreateDeliveryRequest;
+import com.backend.warehouse_management.dto.manager.DeliveryDTO;
 import com.backend.warehouse_management.enums.OrderStatus;
 
 import java.util.List;
 
 public interface OrderService {
 
-    OrderDTO createNewOrder(Long id, OrderDTO orderDTO);
-
-    OrderDTO addItemToOrder(Long userId, OrderItemDTO orderItemDTO);
-
     List<OrderDTO> getAllOrdersForClientId(Long userId);
 
     List<OrderDTO> getOrdersByStatusAndClientId(Long userId, OrderStatus orderStatus);
+    OrderDTO clientCreateOrder(Long userId) throws Exception;
+    OrderDTO clientAddItemToOrder(Long userId, AddItemToOrderRequest itemRequest);
+    OrderDTO clientUpdateItemQuantity(UpdateOrderItemRequest request);
+    OrderDTO clientRemoveItemFromOrder(RemoveOrderItemRequest removeOrderItemRequest);
+    OrderDTO clientSubmitOrder(Long orderId);
+    OrderDTO clientCancelOrder(Long orderId);
+    List<OrderDTO> managerGetAllOrders();
+    OrderDTO managerGetDetailedOrder(Long orderId) throws Exception;
+    OrderDTO managerApproveOrder(Long orderId) throws Exception;
+    OrderDTO managerDeclineOrder(Long orderId, String declineReason) throws Exception;
+    List<DeliveryDTO> managerCheckAvailableDeliveryDates();
+    DeliveryDTO managerCreateDeliveryWithTruck(CreateDeliveryRequest deliveryRequest, Long truckId) throws Exception;
+    DeliveryDTO managerAddOrderToDelivery(Long orderId, Long deliveryId) throws Exception;
+    List<DeliveryDTO> managerRemoveOrderFromDelivery(Long orderId, Long deliveryId);
+
 }
--- src/main/java/com/backend/warehouse_management/service/impl/OrderServiceImpl.java
+++ src/main/java/com/backend/warehouse_management/service/impl/OrderServiceImpl.java
@@ -1,12 +1,14 @@
 package com.backend.warehouse_management.service.impl;
 
-import com.backend.warehouse_management.dto.client.OrderDTO;
-import com.backend.warehouse_management.dto.client.OrderItemDTO;
+import com.backend.warehouse_management.dto.client.*;
+import com.backend.warehouse_management.dto.manager.CreateDeliveryRequest;
+import com.backend.warehouse_management.dto.manager.DeliveryDTO;
 import com.backend.warehouse_management.entity.Order;
 import com.backend.warehouse_management.enums.OrderStatus;
 import com.backend.warehouse_management.mapper.CustomOrderMapper;
 import com.backend.warehouse_management.repository.OrderRepository;
 import com.backend.warehouse_management.service.OrderService;
+import com.backend.warehouse_management.utils.OrderUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,14 +21,79 @@ public class OrderServiceImpl implements OrderService {
 
 
     private final OrderRepository orderRepository;
+    private final OrderUtils orderUtils;
+
     @Override
-    public OrderDTO createNewOrder(Long id, OrderDTO orderDTO) {
-        return null;
+    public OrderDTO clientCreateOrder(Long userId) throws Exception {
+        return orderUtils.clientCreateOrder(userId);
+    }
+
+
+    @Override
+    public OrderDTO clientAddItemToOrder(Long userId, AddItemToOrderRequest itemRequest) {
+        return orderUtils.clientAddItemToOrder(userId, itemRequest);
+    }
+
+
+    @Override
+    public OrderDTO clientUpdateItemQuantity(UpdateOrderItemRequest request) {
+        return orderUtils.clientUpdateItemQuantity(request);
+    }
+
+
+    @Override
+    public OrderDTO clientRemoveItemFromOrder(RemoveOrderItemRequest request) {
+        return orderUtils.clientRemoveItemFromOrder(request);
     }
 
     @Override
-    public OrderDTO addItemToOrder(Long userId, OrderItemDTO orderItemDTO) {
-        return null;
+    public OrderDTO clientSubmitOrder(Long orderId) {
+        return orderUtils.clientSubmitOrder(orderId);
+    }
+
+    @Override
+    public OrderDTO clientCancelOrder(Long orderId) {
+        return orderUtils.clientCancelOrder(orderId);
+    }
+
+    @Override
+    public List<OrderDTO> managerGetAllOrders() {
+        return orderUtils.managerGetAllOrders();
+    }
+
+    @Override
+    public OrderDTO managerGetDetailedOrder(Long orderId) throws Exception {
+        return orderUtils.managerGetDetailedOrder(orderId);
+    }
+
+    @Override
+    public OrderDTO managerApproveOrder(Long orderId) throws Exception {
+        return orderUtils.managerApproveOrder(orderId);
+    }
+
+    @Override
+    public OrderDTO managerDeclineOrder(Long orderId, String declineReason) throws Exception {
+        return orderUtils.managerDeclineOrder(orderId, declineReason);
+    }
+
+    @Override
+    public List<DeliveryDTO> managerCheckAvailableDeliveryDates() {
+        return orderUtils.managerCheckAvailableDeliveryDates();
+    }
+
+    @Override
+    public DeliveryDTO managerCreateDeliveryWithTruck(CreateDeliveryRequest deliveryRequest, Long truckId) throws Exception {
+        return orderUtils.managerCreateDeliveryWithTruck(deliveryRequest, truckId);
+    }
+
+    @Override
+    public DeliveryDTO managerAddOrderToDelivery(Long orderId, Long deliveryId) throws Exception {
+        return orderUtils.managerAddOrderToDelivery(orderId, deliveryId);
+    }
+
+    @Override
+    public List<DeliveryDTO> managerRemoveOrderFromDelivery(Long orderId, Long deliveryId) {
+        return orderUtils.managerRemoveOrderFromDelivery(orderId, deliveryId);
     }
 
     @Override
