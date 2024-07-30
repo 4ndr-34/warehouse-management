abe1df1d37f7966e6595a279587c753dc028e64e
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-07-30 19:45:38 +0200

    fixed LazyInitializationException for hibernate

--- src/main/java/com/backend/warehouse_management/utils/DeliveryScheduler.java
+++ src/main/java/com/backend/warehouse_management/utils/DeliveryScheduler.java
@@ -4,14 +4,16 @@ import com.backend.warehouse_management.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class DeliveryScheduler {
 
     private final OrderService orderService;
 
-    @Scheduled(cron = "0 30 19 * * 1-5")
+    @Scheduled(cron = "0 45 19 * * 1-5")
     public void deliveryScheuling() {
         orderService.completeDelivery();
     }
