90ade0219da62bb50169cfede376d8d88e8f4757
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2024-07-31 23:55:15 +0200

    swagger endpoint ignoring from security

--- src/main/java/com/backend/warehouse_management/security/SecurityConfig.java
+++ src/main/java/com/backend/warehouse_management/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -68,5 +69,11 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(
+                "/swagger-ui/**", "/v3/api-docs/**"
+        );
+    }
 
 }
